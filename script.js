e5ed77c484f16441d6d286b65de0bc32f63ba6bf
CodesByIsmalil <ismailolakunle1401@gmail.com>
2025-10-16 01:07:47 +0100

    new updates

---
+++ script.js
@@ -0,0 +1,10 @@
+const openToggle = document.getElementById('menu-open');
+const closeToggle = document.getElementById('menu-close');
+const navMenu = document.querySelector('.nav__list');
+
+openToggle.addEventListener('click', function () {
+    navMenu.style.display = 'block';
+});
+closeToggle.addEventListener('click', function () {
+    navMenu.style.display = 'none';
+});
